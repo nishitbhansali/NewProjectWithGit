e6243997859f80995bdc98655bbfa193b6df92d3
nishit bhansali <nishit.bhansali@cgi.com>
2024-06-17 17:53:02 -0400

    new feature added

--- src/com/nishit/banking/Transaction.java
+++ src/com/nishit/banking/Transaction.java
@@ -3,4 +3,8 @@ package com.nishit.banking;
 public class Transaction {
 
   private int transactionId;
+  
+  private void updateTransaction() {
+	  System.out.println("This is a transaction");
+  }
 }
